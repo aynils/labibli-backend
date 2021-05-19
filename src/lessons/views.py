dc268845f7cfccec954de5682fa9d7ebd63ecb33
Séraphin Vandegar <svandegar@hotmail.com>
2021-05-19 18:25:59 -0300

    API - Implement JWT token

--- src/lessons/views.py
+++ src/lessons/views.py
@@ -1,12 +1,17 @@
-from rest_framework.decorators import api_view
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_lessons(request):
     return Response({"lessons": ["Hello, world!", "Second lesson"]})
 
+
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_lesson(request):
     return Response({"lesson": {
         "title": "This is the title"
