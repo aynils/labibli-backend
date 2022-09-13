2e874d84101f7890a672c4f0aa6f5c5ee9dc389d
Seraphin <svandegar@hotmail.com>
2022-09-13 14:49:52 -0300

    COLLECTION and BOOKS - Set number of results to 24

--- config/settings/base.py
+++ config/settings/base.py
@@ -138,7 +138,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
 }
 
-DEFAULT_BOOK_PAGE_SIZE = 2
+DEFAULT_BOOK_PAGE_SIZE = 24
 
 AUTH_EMAIL_VERIFICATION = False
 
